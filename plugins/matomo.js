6b293e4d51c1d14ac895beadc02bd31594e898a3
vitaminwater <stantin04@gmail.com>
2019-07-13 11:36:02 +0200

    added matomo analytics

--- plugins/matomo.js
+++ plugins/matomo.js
@@ -1,18 +1,14 @@
 export default ({ app }) => {
   var _paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
-  _paq.push(['enableHeartBeatTimer']);
   (function() {
-    var u="https://analytics.supergreenlab.com/";
+    var u="//analytics.supergreenlab.com/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-
-    app.router.afterEach((to, from) => {
-      _paq.push(['trackPageView']);
-    })
   })();
 }
