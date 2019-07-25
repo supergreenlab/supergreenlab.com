5a303b786b658e4ea73cd94c0a305ca2d1fded78
vitaminwater <stantin04@gmail.com>
2019-07-25 10:44:13 +0200

    mobile we app meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Transform any furniture into a connected growbox.' }
+      { hid: 'description', name: 'description', content: 'Transform any furniture into a connected growbox.' },
+      { name: "apple-mobile-web-app-capable", content:"yes"}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
