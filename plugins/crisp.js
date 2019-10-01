596c59880cd962a785519346c8339bdedbb0f940
vitaminwater <stantin04@gmail.com>
2019-10-01 14:02:27 +0200

    added crisp.chat for on-site support

---
+++ plugins/crisp.js
@@ -0,0 +1,11 @@
+export default ({ app }) => {
+  window.$crisp=[];
+  window.CRISP_WEBSITE_ID="6896c73c-2437-44aa-bf22-c80d02a9e818";
+  (function(){
+    const d=document;
+    const s=d.createElement("script");
+    s.src="https://client.crisp.chat/l.js";
+    s.async=1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+  })();
+}
