e46f31a96a96a3a2cea331ed04990d5bc564cd46
vitaminwater <stantin04@gmail.com>
2019-10-22 11:54:22 +0200

    better title tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'SuperGreenLab - Growshop - Automated LED Grow Lights - All included bundles',
+    title: 'SuperGreenLab - Automated LED Grow Lights for ninja growers',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
