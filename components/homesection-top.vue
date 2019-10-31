07fff30d52290a9a454167191ceb3ae602d62da5
vitaminwater <stantin04@gmail.com>
2019-10-31 09:23:46 +0100

    top animation follow

--- components/homesection-top.vue
+++ components/homesection-top.vue
@@ -124,7 +124,7 @@ export default {
       this.interval = setInterval(() => {
         this.$data.n = this.$data.n + 1
         if (this.$props.focus) {
-          this.$matomo && this.$matomo.trackEvent('front-page', 'top-animation', `image-${this.$data.n}`)
+          this.$matomo && this.$matomo.trackEvent('front-page', 'top-animation', `image-${this.$data.n % 9 + 1}`)
         }
       }, 1500)
     }, 1000)
