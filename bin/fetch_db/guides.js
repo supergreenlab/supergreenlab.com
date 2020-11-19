6bf298e7cb799287bf8a87bb86a574ddcf6c9656
vitaminwater <stantin04@gmail.com>
2020-11-19 10:57:07 +0100

    guide sections ordering

--- bin/fetch_db/guides.js
+++ bin/fetch_db/guides.js
@@ -11,17 +11,17 @@ module.exports.fetchGuides = async () => {
   } catch(e) {}
 
   const guides = await fetchTable('Guides', ['slug', 'thumbnail', 'title', 'subtitle', 'introduction', 'requires', 'sections', 'name', 'medias'])
-  const guideSections = await fetchTable('GuideSections', ['slug', 'title', 'text', 'media', 'requires'])
+  const guideSections = await fetchTable('GuideSections', ['slug', 'title', 'text', 'media', 'requires', 'order'])
   let picPromise = Promise.resolve()
   const guidesJSON = JSON.stringify({
     guides: guides.map(g => {
-    g.sections = guideSections.filter(gs => g.sections.indexOf(gs.id) != -1).map(gs => {
-      const { p, data } = fetchAttachement(picPromise, gs.media[0], 'guides')
-      picPromise = p
-      gs.media = data
-      return gs
-    })
-    return g
+      g.sections = guideSections.filter(gs => g.sections.indexOf(gs.id) != -1).map(gs => {
+        const { p, data } = fetchAttachement(picPromise, gs.media[0], 'guides')
+        picPromise = p
+        gs.media = data
+        return gs
+      }).sort((gs1, gs2) => gs1.order - gs2.order)
+      return g
     })
   })
   await picPromise
