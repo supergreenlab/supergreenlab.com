6c02324a6f6db16588c26fa66de22c613861ce1a
vitaminwater <stantin04@gmail.com>
2020-12-02 14:39:15 +0100

    fixed data importer

--- bin/fetch_db/guides.js
+++ bin/fetch_db/guides.js
@@ -4,17 +4,26 @@ const { fetchTable, fetchAttachement, emptyAssetsDir, mkAssetsDir } = require('.
 
 module.exports.fetchGuides = async () => {
   await mkAssetsDir('guides')
-  let guides = await fetchTable('Guides', ['slug', 'thumbnail', 'title', 'subtitle', 'introduction', 'requires', 'sections', 'name', 'media', 'next'])
+  let guides = await fetchTable('Guides', ['slug', 'thumbnail', 'title', 'subtitle', 'text', 'requires', 'sections', 'name', 'media', 'next'])
   const guideSections = await fetchTable('GuideSections', ['slug', 'title', 'text', 'media', 'requires', 'order'])
   let picPromise = Promise.resolve()
   guides = guides.map(g => {
-    const { p, data } = fetchAttachement(picPromise, g.media[0], 'guides')
-    picPromise = p
-    g.media = data
-    g.sections = guideSections.filter(gs => g.sections.indexOf(gs.id) != -1).map(gs => {
-      const { p, data } = fetchAttachement(picPromise, gs.media[0], 'guides')
+    try {
+      const { p, data } = fetchAttachement(picPromise, g.media[0], 'guides')
       picPromise = p
-      gs.media = data
+      g.media = data
+    } catch(e) {
+      console.log(e)
+    }
+    g.introduction = g.text
+    g.sections = guideSections.filter(gs => g.sections && g.sections.indexOf(gs.id) != -1).map(gs => {
+      try {
+        const { p, data } = fetchAttachement(picPromise, gs.media[0], 'guides')
+        picPromise = p
+        gs.media = data
+      } catch(e) {
+        console.log(e)
+      }
       return gs
     }).sort((gs1, gs2) => gs1.order - gs2.order)
     return g
