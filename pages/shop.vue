82a9f36fc5a2288d1c9a4a0bef1c339523b7bd6d
Nicolas Freche Medina <nicolas.freche@gmail.com>
2021-05-21 15:48:42 +0200

    bundle section done with responsive, refactoring others shops section with responsive

--- pages/shop.vue
+++ pages/shop.vue
@@ -24,27 +24,84 @@
       <div :id='$style.fullcontent'>
         <div :id='$style.leftcolumn'>
           <Banner/>
+          <Newsletter/>
         </div>
         <div :id='$style.content'>
-
+          <div :class='$style.title'>
+            <SectionTitle title='Bundles'/>
+          </div>
+          <div :id='$style.bundles'>
+            <div :class='$style.bundle' v-for='b in bundles' :key='b.id' :ref='b.slug'>
+              <div :id='b.slug'></div>
+              <Bundle :bundle='b' :showdescription='false' :showAll='false' />
+            </div>
+          </div>
+          <div :class='$style.title'>
+            <SectionTitle title='SGL Products'/>
+          </div>
+          <div :class='$style.shop'>
+           <ProductList :products='sglSpareParts' :center=true :maxItems=4 />
+          </div>
           <Banner/>
+          <div :class='$style.title'>
+            <SectionTitle title='Growing pack'/>
+          </div>
+          <div :class='$style.pack'>
+            <Pack />
+          </div>
+          <div :class='$style.title'>
+            <SectionTitle title='Furniture'/>
+          </div>
+          <div :class='$style.shop'>
+            <ProductList ref='homepage-furniture' :products='furnitures' :center=true :maxItems=4 />
+          </div>
+          <div :class='$style.title'>
+            <SectionTitle title='Tools'/>
+          </div>
+          <div :class='$style.shop'>
+            <ProductList ref='homepage-tools' :products='tools' :center=true :maxItems=4 />
+          </div>
           <Newsletter/>
+          <Footer/>
         </div>
         <div :id='$style.rightcolumn'>
           <Newsletter/>
+          <Banner/>
         </div>
       </div>
   </section>
 </template>
 
 <script>
+import SectionTitle from '~/components/widgets/sectiontitle.vue'
+import TitleStep from '~/components/widgets/titlestep.vue'
+import Footer from '~/components/layout/footer.vue'
 import Header from '~/components/shop/layout/header.vue'
 import Banner from '~/components/shop/widgets/banner.vue'
 import SideBar from '~/components/shop/layout/sidebar.vue'
 import Newsletter from '~/components/shop/widgets/newsletter.vue'
+import ProductList from '~/components/products/productlist.vue'
+import Pack from '~/components/home/pack.vue'
+import Bundle from '~/components/shop/layout/sectionbundles.vue'
 
 export default{
-  components: {Header, Banner, SideBar, Newsletter}
+  components: {TitleStep, SectionTitle ,ProductList, Bundle, Pack ,Footer, Header, Banner, SideBar, Newsletter},
+
+  computed: {
+    bundles() {
+			return this.$store.getters['eshop/bundles']
+		},
+    sglSpareParts() {
+      return this.$store.getters['eshop/leds'].concat(this.$store.getters['eshop/accessories'])
+    },
+    furnitures() {
+      return this.$store.getters['eshop/collection']('homepage-furniture')
+    },
+    tools() {
+      return this.$store.getters['eshop/productsWithTypes'](['TOOLS'])
+    },
+	},
+
 }
 </script>
 
@@ -82,8 +139,8 @@ export default{
   margin: 0px 220px
   width: 100%
   height: 100%
-  background-color: grey
-  @media only screen and (max-width: 600px)
+  background-color: #fff
+  @media only screen and (max-width: 1200px)
     margin 20px 0px
 
 #leftcolumn
@@ -92,7 +149,8 @@ export default{
   left: 0
   width: 220px
   height: 100%
-  @media only screen and (max-width: 600px)
+  font-size: 0.7em
+  @media only screen and (max-width: 1200px)
     display: none
 
 #rightcolumn
@@ -102,8 +160,42 @@ export default{
   width: 220px
   height: 100%
   font-size: 0.7em
-  @media only screen and (max-width: 600px)
+  @media only screen and (max-width: 1200px)
     display: none
 
+
+.shop
+  display: flex
+  width: 100%
+  max-width: 900pt
+  padding: 0 15pt
+
+.pack
+  width: 80%
+  display: flex
+  justify-content: center
+
+#bundles
+  display: flex
+  justify-content: space-around
+  align-items: center
+  width: 100%
+  @media only screen and (max-width: 600px)
+    flex-direction: column
+
+.bundle
+  margin: 10px
+  width: 100%
+  /* max-width: 920pt */
+  height: 100%
+  /* border-radius: 10px
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px; */
+  @media only screen and (max-width: 600px)
+    width: 100%
+    height: 100%
+
+.title
+  width: 100%
+  margin: 20pt 0 20pt 0
 
 </style>
