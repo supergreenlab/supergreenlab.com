944077351e81e524292deb3af92f38ba4eebcae2
vitaminwater <stantin04@gmail.com>
2021-12-13 17:16:36 +0100

    added /charity page

---
+++ pages/charity.vue
@@ -0,0 +1,43 @@
+<!--
+      Copyright (C) 2021  SuperGreenLab <towelie@supergreenlab.com>
+      Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -->
+
+<template>
+  <section :id="$style.container">
+  </section>
+</template>
+
+<script>
+
+export default {
+  mounted() {
+    window.location.href = 'https://www.paypal.com/donate/?hosted_button_id=E868UJS9QNYL6'
+  },
+}
+</script>
+
+<style module lang=stylus>
+
+#container
+  display: flex
+  width: 100vw
+  height: 100vh
+  flex-direction: column
+  justify-content: center
+  align-items: center
+
+</style>
