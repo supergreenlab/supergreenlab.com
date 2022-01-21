3f6f90fe64212f0fe12fe1be29334a0f09a431aa
vitaminwater <stantin04@gmail.com>
2022-01-21 09:42:23 +0100

    better region selector

--- components/products/region.vue
+++ components/products/region.vue
@@ -18,19 +18,22 @@
 
 <template>
   <section :id='$style.container'>
-    <div v-if='expanded' :id='$style.regions'>
-      <div
-        :class='$style.region + " " + (i !== regions.length - 1 ? $style.rightborder : "")'
-        :id='region.code == r.code ? $style.selected : ""'
-        v-for='(r, i) in regions'
-        :key='r.id'
-        @click='setRegion(r)'>
-        <div :class='$style.flag' :style='{"background-image": `url(/img/${r.flag.fileSmall})`}'></div>
-        {{ r.code }}
+    <div v-if='expanded' :id='$style.selector'>
+      <span>search: </span><input type='text' v-model='search' :id='$style.search' />
+      <div :id='$style.regions'>
+        <div
+          :class='$style.region + " " + (i !== regions.length - 1 ? $style.rightborder : "")'
+          :id='region.code == r.code ? $style.selected : ""'
+          v-for='(r, i) in regions'
+          :key='r.id'
+          @click='setRegion(r)'>
+          <div :class='$style.flag' :style='{"background-image": `url(/img/${r.flag.fileSmall})`}'></div>
+          {{ r.code }}
+        </div>
       </div>
     </div>
     <div v-else>
-      <div :id='$style.regions'>
+      <div :id='$style.current'>
         <div :class='$style.region' :id='$style.selected' @click='expanded = true'>
           <div :class='$style.flag' :style='{"background-image": `url(/img/${region.flag.fileSmall})`}'></div>
           <p>
@@ -48,7 +51,8 @@
 export default {
   data() {
     return {
-      expanded: false
+      expanded: false,
+      search: '',
     }
   },
   methods: {
@@ -63,7 +67,10 @@ export default {
       return this.$store.state.eshop.region
     },
     regions() {
-      return this.$store.getters['eshop/availableRegions']
+      const { search } = this.$data
+      const regions = this.$store.getters['eshop/availableRegions']
+      if (!search) return regions
+      return regions.filter(r => r.name.toLowerCase().includes(search.toLowerCase()) || r.code.toLowerCase().includes(search.toLowerCase()))
     },
   }
 }
@@ -73,45 +80,58 @@ export default {
 
 #container
   display: flex
-  height: 45pt
   color: #909090
+
+#selector
+  border: 2pt solid #dedede
+  border-radius: 4pt
+  padding: 5px 5px 0 5px
 
 #regions
   display: flex
+  max-width: 300px
+  overflow-x: auto
+  margin-bottom: 3px
+
+#current
   border: 2pt solid #dedede
   border-radius: 4pt
-  margin-right: 3pt
-  max-width: 97vw
-  overflow-x: auto
 
 .region
   display: flex
   align-items: center
   justify-content: center
-  padding: 2pt
+  padding: 5pt
   cursor: pointer
   font-weight: bold
   opacity: 0.5
   transition: opacity 0.2s
+  margin-right: 5px
 
 .flag
-  width: 15pt
-  height: 15pt
+  width: 20pt
+  height: 20pt
   margin-right: 2pt
+  padding: 4px
   border-radius: 3pt
   background-position: center
   background-size: cover
   background-repeat: no-repeat
   background-color: rgba(0, 0, 0, 0.5)
 
-.region:hover
+#regions > .region:hover
   background-color: #dfdfdf
-
-.rightborder
-  border-right: 1pt solid #dfdfdf
+  border-radius: 3px
 
 #selected
   color: #565656
   opacity: 1
+
+#search
+  margin: 3px
+  height: 30px
+  width: 100px
+  border: 2px solid #cdcdcd
+  border-radius: 4px
 
 </style>
