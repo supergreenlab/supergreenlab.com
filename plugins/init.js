d5a270d3661ad12ba52a233f0419cd14348ba6a5
vitaminwater <stantin04@gmail.com>
2022-07-12 15:54:42 +0200

    fixed broken promo link

--- plugins/init.js
+++ plugins/init.js
@@ -1,10 +1,11 @@
 export default async ({ store }) => {
   addEventListener('load', async () => {
     try {
+      console.log(window.location.search)
       const urlParams = new URLSearchParams(window.location.search)
       if (urlParams.get('promo')) {
         const sglSellerIDs = [process.env.sglSellerID, process.env.sgteuSellerID, process.env.sgtusSellerID]
-        await Promise.all(sglSellerIDs.map(sglSellerID => this.$store.dispatch('checkout/fetchPromocode', {promocode: urlParams.get('promo'), sellerid: sglSellerID})))
+        await Promise.all(sglSellerIDs.map(sglSellerID => store.dispatch('checkout/fetchPromocode', {promocode: urlParams.get('promo'), sellerid: sglSellerID})))
       }
     } catch(e) {
       console.log(e)
