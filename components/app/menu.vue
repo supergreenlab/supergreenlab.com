3d45dc9a77fae29defe9346141c04ee033e6da78
vitaminwater <stantin04@gmail.com>
2023-11-02 06:51:06 +0100

    video optimized

---
+++ components/app/menu.vue
@@ -0,0 +1,52 @@
+<!--
+      Copyright (C) 2023  SuperGreenLab <towelie@supergreenlab.com>
+      Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -->
+
+<template>
+  <section :id='$style.container'>
+    <div :class='$style.title'>
+      <SectionTitle title='ALL-IN-ONE **FEATURE** SET'
+                      center='true'
+                      :border='false'/>
+    </div>
+
+  </section>
+</template>
+
+<script>
+
+import SectionTitle from '~/components/widgets/sectiontitle.vue'
+
+export default {
+  components: {SectionTitle, },
+}
+
+</script>
+
+<style module lang=stylus>
+
+#container
+  display: flex
+
+.title
+  font-family: Roboto
+  width: 100%
+  margin: 30pt 0 30pt 0
+  @media only screen and (max-width: 600px)
+    margin: 0
+
+</style>
